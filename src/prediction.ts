47f0e3a93c9f5d9556a390946ef57e3162c7c456
Miljan9602 <rakitamiljan@yahoo.com>
2024-11-22 10:59:03 +0900

    redeploy version 1.0.7

--- src/prediction.ts
+++ src/prediction.ts
@@ -78,7 +78,7 @@ export function handleClaim(event: ClaimEvent): void {
   let entity = Game.load(gameId)
 
   if (entity === null) {
-    console.log('Game not found for claim with gameId: '+gameId+' -> data:' + event.params.epoch.toString() + ' instance: ' + event.address.toHexString() + ' sender: ' + event.params.sender.toHexString())
+    // console.log('Game not found for claim with gameId: '+gameId+' -> data:' + event.params.epoch.toString() + ' instance: ' + event.address.toHexString() + ' sender: ' + event.params.sender.toHexString())
     return;
     // throw new Error('Game not found for claim with gameId: '+gameId+' -> data:' + event.params.epoch.toString() + ' instance: ' + event.address.toHexString() + ' sender: ' + event.params.sender.toHexString())
   }
